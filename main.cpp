0d19eff2a8af1805c09240cc0d416de63f1af776
Monica Bagnasco <s309387@studenti.polito.it>
2025-04-16 18:25:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Utils.hpp"
+#include "PolygonalMesh.hpp"
+#include <limits>
+
+using namespace std;
+using namespace MeshLibrary;
+
+
+int main()
+{
+    PolygonalMesh mesh;
+    string path = "PolygonalMesh";
+    double tol = 1e-6;
+
+    if(!importMesh(path, mesh, tol))
+        return 1;
+
+    return 0;
+}
